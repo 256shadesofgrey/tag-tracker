a03514630bb77a8c4e634534932a31e21cb664de
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2025-05-24 16:24:56 +0200

    Capture and display video stream

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
-int main(int argc, char* argv[]) {
-  std::cout<<"Hello, World!"<<std::endl;
+int main(int argc, char *argv[]) {
+  std::string video_source = "";
+
+  if (argc < 2) {
+    video_source = "http://192.168.178.10:8080/video";
+    std::cout << "No video source was given. Using the default " << video_source
+              << std::endl;
+  } else {
+    video_source = std::string(argv[1]);
+    std::cout << "Using the following source: " << video_source << std::endl;
+  }
+
+  cv::VideoCapture cap(video_source);
+
+  if (!cap.isOpened()) {
+    std::cerr << "Error: Could not open IP camera at " << video_source << "."
+              << std::endl;
+    return -1;
+  }
+
+  cv::namedWindow("Camera Feed", cv::WINDOW_NORMAL);
+
+  cv::Mat frame;
+
+  while (true) {
+    cap >> frame;
+
+    if (frame.empty()) {
+      std::cerr << "Error: Could not read frame." << std::endl;
+      break;
+    }
+
+    cv::imshow("Camera Feed", frame);
+
+    // Wait for X milliseconds. If a key is pressed, break from the loop.
+    if (cv::waitKey(1) >= 0)
+      break;
+  }
+
+  // Release the VideoCapture object and close all windows
+  cap.release();
+  cv::destroyAllWindows();
+
   return 0;
 }
