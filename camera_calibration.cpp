638cf823c014487b3fda03157a2e803bb37f2075
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2025-08-24 17:45:17 +0200

    Improve window management and verbosity levels

--- camera_calibration.cpp
+++ camera_calibration.cpp
@@ -9,6 +9,13 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+// Change to 1 to attempt screen size detection for window arrangement.
+// Disabled by default because it does not seem to work consistently.
+// Use --sw and --sh command line options instead.
+#ifndef SCREEN_SIZE_DETECTION
+#define SCREEN_SIZE_DETECTION 0
+#endif
+
 namespace po = boost::program_options;
 
 int main(int argc, char *argv[]) {
@@ -16,26 +23,39 @@ int main(int argc, char *argv[]) {
   std::string path = "../calibration/*.jpg";
   int checkerboardWidth = 8;
   int checkerboardHeight = 5;
+  int windowWidth = 960;
+  int windowHeight = 540;
+  int screenWidth = 3840;
+  int screenHeight = 2160;
+  int autoarrange = 0;
 
   po::options_description desc("Available options");
 
   desc.add_options()
     ("help,h", "Show this message.")
-    ("verbose,v", "Display additional information.")
+    ("verbose,v", po::value<int>()->default_value(0)->implicit_value(1), "Display additional information. Higher value gives additional output.")
     ("width,W", po::value<int>(), std::format("Number of inner corners horizontally (i.e. columns-1). (Default: {})", checkerboardWidth).c_str())
     ("height,H", po::value<int>(), std::format("Number of inner corners vertically (i.e. rows-1). (Default: {})", checkerboardHeight).c_str())
     ("images,i", po::value<std::string>(), std::format("Output folder. (Default: {})", path).c_str())
+    ("ww", po::value<int>(), std::format("Width of the image display windows. (Default: {})", windowWidth).c_str())
+    ("wh", po::value<int>(), std::format("Height of the image display windows. (Default: {})", windowHeight).c_str())
+    ("autoarrange,a", "Arrange windows to optimally fill the screen. This does not work on wayland.")
+    ("sw", po::value<int>(), std::format("Width of the screen. (Default: {})", screenWidth).c_str())
+    ("sh", po::value<int>(), std::format("Height of the screen. (Default: {})", screenHeight).c_str())
   ;
 
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
 
   if (vm.count("help")) {
     std::cout << desc << std::endl;
     return 1;
   }
 
-  verbosity = vm.count("verbose");
+  if (vm.count("verbose")) {
+    verbosity = vm["verbose"].as<int>();
+  }
 
   if (vm.count("width")) {
     checkerboardWidth = vm["width"].as<int>();
@@ -49,10 +69,57 @@ int main(int argc, char *argv[]) {
     path = vm["images"].as<std::string>();
   }
 
+  if (vm.count("ww")) {
+    windowWidth = vm["ww"].as<int>();
+  }
+
+  if (vm.count("wh")) {
+    windowHeight = vm["wh"].as<int>();
+  }
+
+  autoarrange = vm.count("autoarrange");
+
+  if (vm.count("sw")) {
+    screenWidth = vm["sw"].as<int>();
+  }
+
+  if (vm.count("sh")) {
+    screenHeight = vm["sh"].as<int>();
+  }
+
+#if SCREEN_SIZE_DETECTION
+  cv::namedWindow("screen", cv::WND_PROP_FULLSCREEN);
+  cv::setWindowProperty("screen", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
+  cv::waitKey(10);
+  cv::Rect screen = cv::getWindowImageRect("screen");
+  cv::destroyWindow("screen");
+
+  if (verbosity > 0) {
+    std::cout << "Detected screen size: " << screen.width << "x" << screen.height << std::endl;
+  }
+
+  if (!vm.count("sw")) {
+    screenWidth = screen.width;
+  }
+
+  if (!vm.count("sh")) {
+    screenHeight = screen.height;
+  }
+#endif // SCREEN_SIZE_DETECTION
+
   if (verbosity > 0) {
     std::cout << "Setting width to: " << checkerboardWidth << std::endl;
     std::cout << "Setting height to: " << checkerboardHeight << std::endl;
     std::cout << "Calibration images: " << path << std::endl;
+    std::cout << "Window width: " << windowWidth << std::endl;
+    std::cout << "Window height: " << windowHeight << std::endl;
+    std::cout << "Autoarrange windows: " << autoarrange << std::endl;
+    std::cout << "Screen width: " << screenWidth << std::endl;
+    std::cout << "Screen height: " << screenHeight << std::endl;
+  }
+
+  if (verbosity > 1) {
+    std::cout << "Build info: " << std::endl << cv::getBuildInformation() << std::endl;
   }
 
   std::vector<std::vector<cv::Point3f> > objpoints;
@@ -73,9 +140,18 @@ int main(int argc, char *argv[]) {
   std::vector<cv::Point2f> corner_pts;
   bool success;
 
-  cv::namedWindow("Image", cv::WINDOW_NORMAL);
+  int winCols = 0;
+  int winRows = 0;
+
+  if (autoarrange) {
+    winCols = screenWidth / windowWidth;
+    winRows = screenHeight / windowHeight;
+  }
 
   for (unsigned int img = 0; img < images.size(); img++) {
+    cv::namedWindow(std::format("Image{}: {}", img, images[img]), cv::WINDOW_NORMAL);
+    cv::resizeWindow(std::format("Image{}: {}", img, images[img]), windowWidth, windowHeight);
+
     frame = cv::imread(images[img]);
     cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
 
@@ -91,9 +167,16 @@ int main(int argc, char *argv[]) {
       imgpoints.push_back(corner_pts);
     }
 
-    cv::imshow("Image", frame);
-    cv::waitKey(0);
+    cv::imshow(std::format("Image{}: {}", img, images[img]), frame);
+    cv::waitKey(1);
+    cv::resizeWindow(std::format("Image{}: {}", img, images[img]), windowWidth, windowHeight);
+
+    if (autoarrange) {
+      cv::moveWindow(std::format("Image{}: {}", img, images[img]), windowWidth * (img%winCols), windowHeight * (img/winCols % winRows));
+    }
   }
+
+  cv::waitKey(0);
 
   cv::destroyAllWindows();
 
